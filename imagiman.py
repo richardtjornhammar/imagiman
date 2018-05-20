369f50ac1e49e5cd22021f2c925e6846594e8374
Richard Tjörnhammar <richard.tjornhammar@gmail.com>
2018-05-20 12:51:19 +0200

    rankdata

--- imagiman.py
+++ imagiman.py
@@ -4,6 +4,7 @@ import matplotlib.image as mpimg
 import matplotlib.cm as cm
 import numpy as np
 from scipy import signal as sg
+from scipy.stats import rankdata
 
 def gaussian_kernel( size, sigma ):
     size	= int(size*0.5)
@@ -18,11 +19,9 @@ def gaussian_bfact ( size, bfact ):
 
 def kurtosis(imgmap):
 	fimgm		= np.mean(imgmap)
-	dev		= imgmap - fimgm 
-	kurt		= np.mean(dev**4)/(np.mean(dev**2))**2	
-#	n,m=imgmap.shape
-#	N=n*m
-	return kurt#*N
+	dev		= imgmap - fimgm
+	kurt		= np.mean(dev**4)/(np.mean(dev**2))**2
+	return kurt
 
 def img_svd_recon(imgmap,i,j):
 	U, s, V 	= np.linalg.svd(imgmap)
@@ -35,11 +34,10 @@ def img_svd_recon(imgmap,i,j):
 def img_B_scale( B, imap ):
 	new_map = imap
 	b_map = np.exp(-B*imap[:,:])
-	#print np.reshape(b_map, (-1,1) )[:,0]
-	if np.isnan( b_map.any() ):
-		print "IS NAN"
+	if np.isnan( b_map.any() ) :
+		print ( "IS NAN" )
 	else:
-		new_map*=b_map
+		new_map *= b_map
 	return new_map
 
 def img_cdf ( imgmap ):
@@ -48,7 +46,7 @@ def img_cdf ( imgmap ):
 	he_n		= n*m*1.0
 	ni_max  	= np.max(int_img)
 	ni_min  	= np.min(int_img)
-	print "INFO::CDFCALC",ni_max,np.max(imgmap)
+	print ("INFO::CDFCALC",ni_max,np.max(imgmap))
 	p		= np.array([(sum(sum(int_img==0)))/he_n])
 	cdf		= p
 	for i in np.nditer( np.arange(0,ni_max,1)) :
@@ -83,17 +81,16 @@ def img_deequalisation(imgmap,p):
 	imax 	= p.shape[0]
 	I_max 	= np.max(imgmap)
 
-	print imax, I_max, p[p.shape[0]-1], p.shape[0]-1
+	print (imax, I_max, p[p.shape[0]-1], p.shape[0]-1)
 
 	int_img = np.round( imgmap * (imax-1.0) / I_max )
-	print np.max(int_img)
+	print (np.max(int_img))
 
 	he_img	= np.zeros( (int_img.shape[0], int_img.shape[1]) )
 	for i in range(0,int_img.shape[0]-1,1) :
 		for j in range(0,int_img.shape[1]-1,1) :
 			he_img[i,j]=p[ int_img[i,j] ]
 	return he_img#*I_max
-
 
 def img_cdf_scale(imgmap,cdf,power):
 	I_max 	= np.max(imgmap)
@@ -105,7 +102,7 @@ def img_cdf_scale(imgmap,cdf,power):
 			he_img[i,j]=int_img[i,j]*val**power
 	return he_img
 
-def transform_cdf(cdf1,cdf2):
+def transform_cdf ( cdf1, cdf2 ):
 	cdf_transform = 0
 	for i in range( 1, cdf1.shape[0] ):
 		J		= np.argmin( np.abs(cdf1[i]-cdf2) )
@@ -129,10 +126,10 @@ def img_svd_denoise_train(imgmap, Nsvd, keep_low):
 	recon_img4	= np.zeros(recon_img1.shape)
 	if keep_low == 1 :
 		for i in range(1,Nsvd):
-			recon_img1	 = img_svd_recon(imgmap,Nsvd,n)	
+			recon_img1	 = img_svd_recon(imgmap,Nsvd,n)
 			recon_img2	 = img_svd_recon(imgmap,0,Nsvd)
-			j		 = i # 2 #Nsvd+1-i 
-			dsp_img		 = sg.convolve( recon_img2 , np.ones((2*j+1,2*j+1))) 
+			j		 = i # 2 #Nsvd+1-i
+			dsp_img		 = sg.convolve( recon_img2 , np.ones((2*j+1,2*j+1)))
 			recon_img2	 = dsp_img[j:n+j,j:n+j]
 			recon_img4	+= (recon_img1/np.max(recon_img1)+recon_img2/np.max(recon_img2)) * I_max
 	else :
@@ -140,15 +137,14 @@ def img_svd_denoise_train(imgmap, Nsvd, keep_low):
 	return recon_img4
 
 def img_non_edge_blur_smooth(imap):
-
 	n,m		= imap.shape
 	imap_o		= imap*0.0;
 	tmp_img		= imap_o
 
 	fimgm		= np.mean(imap)
-	dev		= imap - fimgm 
-	kurt		= np.mean(dev**4)/(np.mean(dev**2))**2	
-	print "INFOINFO::", kurt, np.std(imap)
+	dev		= imap - fimgm
+	kurt		= np.mean(dev**4)/(np.mean(dev**2))**2
+	print ("INFOINFO::", kurt, np.std(imap))
 
 	sigmaI	 	= np.std(imap)**2
 	sigmap		= np.sqrt(kurt)**2
@@ -164,9 +160,7 @@ def img_non_edge_blur_smooth(imap):
 			n_dr_map = np.roll(n_dr_map,i,axis=1)
 			D2		 = i**2+j**2
 			tmp_img		+= np.exp(-0.5*(n_dr_map-imap)**2/sigmaI)*np.exp(-0.5*D2/sigmap)*n_dr_map
-
-	print np.max(tmp_img), np.min(tmp_img)
-
+	print (np.max(tmp_img), np.min(tmp_img))
 	if 0:
 		mat_rand	= np.random.random((3,3))
 		mat_rand[1,1]	= 1.0
@@ -183,9 +177,9 @@ def img_non_edge_blur_smooth_norm(imap):
 	tmp_img		= imap_o
 
 	fimgm		= np.mean(imap)
-	dev		= imap - fimgm 
-	kurt		= np.mean(dev**4)/(np.mean(dev**2))**2	
-	print "INFOINFO::", kurt, np.std(imap)
+	dev		= imap - fimgm
+	kurt		= np.mean(dev**4)/(np.mean(dev**2))**2
+	print ("INFOINFO::", kurt, np.std(imap))
 
 	kurt		= 1.0*kurt
 	sigmaI1	 	= 1.0*np.std(imap)**2
@@ -194,7 +188,7 @@ def img_non_edge_blur_smooth_norm(imap):
 	npix 		= int( round(kurt*5) )
 
 	n_dr_map	= imap_o
-	sq2g= np.sqrt(2.0*np.pi)
+	sq2g = np.sqrt(2.0*np.pi)
 	iG1 	= 1.0/(sq2g*np.sqrt(sigmaI1))
 	iG2 	= 1.0/(sq2g*np.sqrt(sigmaI2))
 	iG3 	= 1.0/(sq2g*np.sqrt(sigmap ))
@@ -211,8 +205,7 @@ def img_non_edge_blur_smooth_norm(imap):
 			W		+= w
 			tmp_img		+= w*n_dr_map
 	tmp_img /= W
-	print np.max(tmp_img), np.min(tmp_img), np.mean(tmp_img)
-
+	print (np.max(tmp_img), np.min(tmp_img), np.mean(tmp_img))
 	return tmp_img
 
 def img_svd_denoise(imgmap, Nsvd, keep_low):
@@ -301,15 +294,15 @@ def img_deriv(A_img):
 	p,q = D_img.shape
 	n,m = A_img.shape
 	fx=round((p-n)*0.5)
-	fy=round((q-m)*0.5)	
+	fy=round((q-m)*0.5)
 	return D_img[fx:n+fx,fy:m+fy]*iamax/idmax
-
-#img 		= mpimg.imread('RichTjorn.bmp');		magnitude 	= 1E+3
+#
+img 		= mpimg.imread('RichTjorn.bmp');		magnitude 	= 1E+3
 #img 		= mpimg.imread('crew89_noisy_25.png');		magnitude 	= 1E-1
-img 		= mpimg.imread('lizard_noisy.bmp');		magnitude 	= 1E-1
+#img 		= mpimg.imread('lizard_noisy.bmp');		magnitude 	= 1E-1
 #img 		= mpimg.imread('noisy_voc_best_002.bmp');	magnitude 	= 1E-1
 #img 		= mpimg.imread('chest_xray.bmp');		magnitude 	= 1E-3
-
+#
 narrimgsh	= np.array(img.shape)
 DIM 		= narrimgsh.shape[0]
 
@@ -321,23 +314,38 @@ else:
 W		= ( rand(n,m) + rand(n,m) + rand(n,m)
 		+   rand(n,m) + rand(n,m) + rand(n,m) )/6.0
 
-print "SNR"
-print np.mean(img)/np.std(W*magnitude)**2
+print ("SNR")
+print (np.mean(img)/np.std(W*magnitude)**2)
 
-if DIM==3:
+if DIM==3 :
 	flat_img	= np.mean(img,2)
 else:
 	flat_img	= img
 
-noisy_img=flat_img+W*magnitude
-noisy_img/=np.max(noisy_img)/256.0
-A_img	= noisy_img
+noisy_img  = flat_img+W*magnitude
+noisy_img /= np.max(noisy_img)/256.0
+
+nm = np.shape(noisy_img)
+linimg = noisy_img.reshape(-1)
+rimg = rankdata(linimg,'average').reshape(nm)/float(len(linimg))
+
+print('RANKED INTENSITY RANGE:', np.min(rimg),np.max(rimg) )
+
+A_img = noisy_img
+#A_img = rimg
 
 figure(1)
 imgplot1 = plt.imshow( noisy_img , cmap = cm.Greys_r )
 
 figure(2)
 neb_img	= img_non_edge_blur_smooth_norm( A_img )
+imgplot1 = plt.imshow( rimg , cmap = cm.Greys_r )
+
+figure(3)
+deb_img = img_non_edge_blur_smooth_norm( rimg )
+imgplot1 = plt.imshow( deb_img , cmap = cm.Greys_r )
+
+figure(4)
 imgplot1 = plt.imshow( neb_img , cmap = cm.Greys_r )
 
 show()
